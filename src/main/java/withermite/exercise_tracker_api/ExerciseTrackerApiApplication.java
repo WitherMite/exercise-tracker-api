f885875a32c82e66b3786c0c2964899f26d08187
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-05-24 23:19:26 -0500

    Initial commit

---
+++ src/main/java/withermite/exercise_tracker_api/ExerciseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package withermite.exercise_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/withermite/exercise_tracker_api/ExerciseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package withermite.exercise_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
