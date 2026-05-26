0ee817651afd3426d5ded7abc0ec4d9556795df6
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-05-25 19:48:46 -0500

    Write initial DB seed script

---
+++ seed-schema.sql
@@ -0,0 +1,58 @@
+BEGIN;
+
+CREATE TYPE user_role_enum AS ENUM ('default', 'admin');
+
+CREATE TABLE IF NOT EXISTS users (
+    id                  int            GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username            varchar(30)    CONSTRAINT user_public_id UNIQUE NOT NULL,
+    displayname         varchar(30)    NOT NULL,
+    pw_hash             text           NOT NULL,
+    user_role           user_role_enum DEFAULT 'default' NOT NULL,
+    weight              real,
+    are_workouts_public boolean        DEFAULT false NOT NULL
+);
+
+CREATE TYPE count_type_enum AS ENUM ('single', 'lap', 'set');
+CREATE TYPE work_time_type_enum AS ENUM ('time', 'repetition');
+CREATE TYPE load_type_enum AS ENUM ('distance', 'weight');
+CREATE TYPE rest_type_enum AS ENUM ('optional', 'beforeCount', 'afterCount');
+
+CREATE TABLE IF NOT EXISTS exercise_types (
+    id                 int                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    exercise_type_name varchar(255)        CONSTRAINT exercise_type_public_id UNIQUE NOT NULL,
+    count_type         count_type_enum     NOT NULL,
+    work_time_type     work_time_type_enum NOT NULL,
+    load_type          load_type_enum      NOT NULL,
+    rest_type          rest_type_enum      DEFAULT 'optional' NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS exercises (
+    id            int          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    exercise_name varchar(255) CONSTRAINT exercise_public_id UNIQUE NOT NULL,
+    description   text,
+    exercise_type int          REFERENCES exercise_types NOT NULL
+);
+
+CREATE TYPE subjective_effort_type_enum AS ENUM ('CR-10', 'Borg');
+
+CREATE TABLE IF NOT EXISTS user_exercises (
+    id                      int                         GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id                 int                         REFERENCES users NOT NULL,
+    exercise_id             int                         REFERENCES exercises NOT NULL,
+    datetime                timestamp                   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    exercise_count          smallint                    DEFAULT 1 NOT NULL,
+    notes                   text,
+    subjective_effort_type  subjective_effort_type_enum DEFAULT 'CR-10' NOT NULL,
+    subjective_effort_value real
+);
+
+CREATE TABLE IF NOT EXISTS user_exercise_statistics (
+    user_exercise_id int      REFERENCES user_exercises NOT NULL,
+    index            smallint NOT NULL,
+    work_time        real,
+    load             real,
+    rest_length      interval,
+    PRIMARY KEY (user_exercise_id, index)
+);
+
+COMMIT;
