81d1acf31ff4fadcfb4f130c3549f30e46faed65
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-05-27 23:06:53 -0500

    Add cascading deletes for user data in db

--- seed-schema.sql
+++ seed-schema.sql
@@ -4,7 +4,7 @@ CREATE TYPE user_role_enum AS ENUM ('default', 'admin');
 
 CREATE TABLE IF NOT EXISTS users (
     id                  int            GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username            varchar(30)    CONSTRAINT user_public_id UNIQUE NOT NULL,
+    username            varchar(30)    CONSTRAINT user_natural_key UNIQUE NOT NULL,
     displayname         varchar(30)    NOT NULL,
     pw_hash             text           NOT NULL,
     user_role           user_role_enum DEFAULT 'default' NOT NULL,
@@ -19,7 +19,7 @@ CREATE TYPE rest_type_enum AS ENUM ('optional', 'beforeCount', 'afterCount');
 
 CREATE TABLE IF NOT EXISTS exercise_types (
     id                 int                 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    exercise_type_name varchar(255)        CONSTRAINT exercise_type_public_id UNIQUE NOT NULL,
+    exercise_type_name varchar(255)        CONSTRAINT exercise_type_natural_key UNIQUE NOT NULL,
     count_type         count_type_enum     NOT NULL,
     work_time_type     work_time_type_enum NOT NULL,
     load_type          load_type_enum      NOT NULL,
@@ -27,16 +27,16 @@ CREATE TABLE IF NOT EXISTS exercise_types (
 );
 
 CREATE TABLE IF NOT EXISTS exercises (
-    id            int          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    exercise_name varchar(255) CONSTRAINT exercise_public_id UNIQUE NOT NULL,
-    exercise_type int          REFERENCES exercise_types NOT NULL,
-    description   text
+    id               int          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    exercise_name    varchar(255) CONSTRAINT exercise_natural_key UNIQUE NOT NULL,
+    exercise_type_id int          REFERENCES exercise_types NOT NULL,
+    description      text
 );
 
 CREATE TABLE IF NOT EXISTS user_exercises (
     id             int       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id        int       REFERENCES users NOT NULL,
-    exercise_id    int       REFERENCES exercises NOT NULL,
+    user_id        int       REFERENCES users ON DELETE CASCADE NOT NULL,
+    exercise_id    int       REFERENCES exercises ON DELETE RESTRICT NOT NULL,
     datetime       timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
     exercise_count smallint  DEFAULT 1 NOT NULL,
     notes          text
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS user_exercises (
 CREATE TYPE subjective_effort_type_enum AS ENUM ('CR-10', 'Borg');
 
 CREATE TABLE IF NOT EXISTS user_exercise_statistics (
-    user_exercise_id        int                         REFERENCES user_exercises NOT NULL,
+    user_exercise_id        int                         REFERENCES user_exercises ON DELETE CASCADE NOT NULL,
     index                   smallint                    NOT NULL,
     work_time               real,
     load                    real,
