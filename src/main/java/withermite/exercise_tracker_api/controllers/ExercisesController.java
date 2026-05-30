5a317ceaa2975102382bd82522e391af821bf938
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-05-30 05:00:44 -0500

    Model request endpoints

---
+++ src/main/java/withermite/exercise_tracker_api/controllers/ExercisesController.java
@@ -0,0 +1,38 @@
+package withermite.exercise_tracker_api.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exercises")
+class ExercisesController {
+    
+    @GetMapping
+    public String many() {
+        return "many";
+    }
+
+    @PostMapping
+    public String add() {
+        return "add";
+    }
+
+    @GetMapping("/{key}")
+    public String one() {
+        return "one";
+    }
+
+    @PutMapping("/{key}")
+    public String update() {
+        return "update";
+    }
+
+    @DeleteMapping("/{key}")
+    public String delete() {
+        return "delete";
+    }
+}
---
+++ src/main/java/withermite/exercise_tracker_api/controllers/RootController.java
@@ -0,0 +1,15 @@
+package withermite.exercise_tracker_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+class RootController {
+    
+    @GetMapping
+    public String respond() {
+        return "hi :3";
+    }
+}
