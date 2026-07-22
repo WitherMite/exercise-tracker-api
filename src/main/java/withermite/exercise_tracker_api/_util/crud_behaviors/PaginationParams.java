5cd3b4170b29ee30dd6284bf40898be75b3f28e2
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-07-22 12:52:33 -0500

    Add messages to pagination parameter constraints

--- src/main/java/withermite/exercise_tracker_api/_util/crud_behaviors/PaginationParams.java
+++ src/main/java/withermite/exercise_tracker_api/_util/crud_behaviors/PaginationParams.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record PaginationParams(
-        @Min(0) @Max(128) Integer limit,
-        @PositiveOrZero Integer offset) {
+        @Min(value = 0, message = "Limit must be between 0 and 128") @Max(value = 128, message = "Limit must be between 0 and 128") Integer limit,
+        @PositiveOrZero(message = "Offset must be greater than 0") Integer offset) {
 
     public int getLimitOrDefault(int defaultLimit) {
         Integer limitParam = limit();
